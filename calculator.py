4a08130d1856bb0f866d369d7e5d466cecc66a7f
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-06-22 21:06:58 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#here i print some message
+print("Welcome to the calculator")
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+#here I take two input from the user and save the input a vairable int
+print("The addition of the two numbers is",a+b)
+print("the subraction of two number is",a-b)
+print("the multipltion of two number is",a*b)
+print("the division of two number is",a/b)
